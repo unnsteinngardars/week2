9c514a8891a96553638c3c4b6349c5f049280b51
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-12-07 23:05:47 +0000

    stuff

--- provisioning/create-aws-docker-host-instance.sh
+++ provisioning/create-aws-docker-host-instance.sh
@@ -32,8 +32,7 @@ else
     SECURITY_GROUP_ID=$(cat ./ec2_instance/security-group-id.txt)
 fi
 
-MY_PRIVATE_IP=$(hostname -I | cut -d' ' -f1)
-MY_PUBLIC_IP=$(curl http://checkip.amazonaws.com)
+
 if [ ! -e ./ec2_instance/instance-id.txt ]; then
     echo Create ec2 instance on security group ${SECURITY_GROUP_ID} ${AMI_IMAGE_ID}
     INSTANCE_INIT_SCRIPT=docker-instance-init.sh
@@ -51,13 +50,16 @@ if [ ! -e ./ec2_instance/instance-public-name.txt ]; then
     echo ${INSTANCE_PUBLIC_NAME} > ./ec2_instance/instance-public-name.txt
 fi
 
-
+MY_PRIVATE_IP=$(hostname -I | cut -d' ' -f1)
+MY_PUBLIC_IP=$(curl http://checkip.amazonaws.com)
 MY_CIDR=${MY_PUBLIC_IP}/32
 MY_PRIVATE_CIDR=${MY_PRIVATE_IP}/32
 ALL=0.0.0.0/0
 
 echo Using CIDR ${MY_CIDR} for access restrictions.
+source ./create-aws-docker-host-instance.sh
 
 set +e
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_PRIVATE_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 8080 --cidr ${ALL}
