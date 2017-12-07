cc7e6d2ee04fb15943a53876a11fac607cd82164
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-12-07 18:44:24 +0000

    creating instance

--- provisioning/delete-aws-docker-host-instance.sh
+++ provisioning/delete-aws-docker-host-instance.sh
@@ -7,14 +7,14 @@ SECURITY_GROUP_NAME=$(cat ./ec2_instance/security-group-name.txt)
 aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
 
 echo Waiting for instance to terminate....
-aws ec2 wait --region eu-west-1 instance-terminated --instance-ids ${INSTANCE_ID}
+aws ec2 wait --region us-east-2 instance-terminated --instance-ids ${INSTANCE_ID}
 echo Instance ${INSTANCE_ID} terminated
 
 rm ./ec2_instance/instance-id.txt
 rm ./ec2_instance/instance-public-name.txt
 
-#aws ec2 delete-security-group --group-id ${SECURITY_GROUP_ID}
+aws ec2 delete-security-group --group-id ${SECURITY_GROUP_ID}
 #
-#aws ec2 delete-key-pair --key-name ${SECURITY_GROUP_NAME}
+aws ec2 delete-key-pair --key-name ${SECURITY_GROUP_NAME}
 #
-#rm  -rf ec2_instance
+rm  -rf ec2_instance
