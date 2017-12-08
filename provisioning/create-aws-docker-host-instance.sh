0dfe85ee35b937bba8ad9fbc0c5fcfe8a196ca77
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-12-08 14:14:45 +0000

    more tilda fixes

--- provisioning/create-aws-docker-host-instance.sh
+++ provisioning/create-aws-docker-host-instance.sh
@@ -19,7 +19,7 @@ echo "Using security group name ${SECURITY_GROUP_NAME}"
 MY_PRIVATE_IP=$(hostname -I | cut -d' ' -f1)
 MY_PUBLIC_IP=$(curl http://checkip.amazonaws.com)
 
-if [ ! -e ./ec2_instance/security-group-name.txt ]; then
+if [ ! -e ~/ec2_instance/security-group-name.txt ]; then
     echo ${SECURITY_GROUP_NAME} > ~/ec2_instance/security-group-name.txt
 fi
 
@@ -28,7 +28,7 @@ if [ ! -e ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem ]; then
     chmod 400 ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem
 fi
 
-if [ ! -e ./ec2_instance/security-group-id.txt ]; then
+if [ ! -e ~/ec2_instance/security-group-id.txt ]; then
     SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ${SECURITY_GROUP_NAME} --description "security group for dev environment in EC2" --query "GroupId"  --output=text)
     echo ${SECURITY_GROUP_ID} > ~/ec2_instance/security-group-id.txt
     echo Created security group ${SECURITY_GROUP_NAME} with ID ${SECURITY_GROUP_ID}
@@ -37,11 +37,11 @@ else
 fi
 
 
-if [ ! -e ./ec2_instance/instance-id.txt ]; then
+if [ ! -e ~/ec2_instance/instance-id.txt ]; then
     echo Create ec2 instance on security group ${SECURITY_GROUP_ID} ${AMI_IMAGE_ID}
     INSTANCE_INIT_SCRIPT=docker-instance-init.sh
     INSTANCE_ID=$(aws ec2 run-instances  --user-data file://${INSTANCE_INIT_SCRIPT} --image-id ${AMI_IMAGE_ID} --security-group-ids ${SECURITY_GROUP_ID} --count 1 --instance-type t2.micro --key-name ${SECURITY_GROUP_NAME} --query 'Instances[0].InstanceId'  --output=text)
-    echo ${INSTANCE_ID} > ./ec2_instance/instance-id.txt
+    echo ${INSTANCE_ID} > ~/ec2_instance/instance-id.txt
 
     echo Waiting for instance to be running
     echo aws ec2 wait --region us-east-2 instance-running --instance-ids ${INSTANCE_ID}
@@ -49,7 +49,7 @@ if [ ! -e ./ec2_instance/instance-id.txt ]; then
     echo EC2 instance ${INSTANCE_ID} ready and available on ${INSTANCE_PUBLIC_NAME}
 fi
 
-if [ ! -e ./ec2_instance/instance-public-name.txt ]; then
+if [ ! -e ~/ec2_instance/instance-public-name.txt ]; then
     export INSTANCE_PUBLIC_NAME=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
     echo ${INSTANCE_PUBLIC_NAME} > ~/ec2_instance/instance-public-name.txt
 fi
