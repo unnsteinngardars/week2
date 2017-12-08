ab4b212121f3c2d3eb365413608c9320328829d2
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-12-08 12:29:48 +0000

    port 80 removed, database.json changed host from localhost to postgres

--- provisioning/create-aws-docker-host-instance.sh
+++ provisioning/create-aws-docker-host-instance.sh
@@ -63,5 +63,4 @@ echo Using CIDR ${MY_CIDR} for access restrictions.
 set +e
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_PRIVATE_CIDR}
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
-aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 80 --cidr ${MY_CIDR}
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 8080 --cidr ${ALL}
