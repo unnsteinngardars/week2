a852da8de4706f769a26ca23368129e0548dfba5
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-12-08 15:17:19 +0000

    ble

--- provisioning/docker-compose-and-run.sh
+++ provisioning/docker-compose-and-run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export GIT_COMMIT=$1
+echo $GIT_COMMIT
 docker-compose down
 docker-compose up -d --build
--- provisioning/docker-instance-init.sh
+++ provisioning/docker-instance-init.sh
@@ -4,6 +4,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 sudo yum -y update
 sudo yum -y install docker
+
 sudo pip install docker-compose
 sudo pip install backports.ssl_match_hostname --upgrade
 
