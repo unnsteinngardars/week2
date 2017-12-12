1a86e1879b1b5587c1851f5f9d96566d346ce860
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-12-12 16:20:23 +0000

    changed port in run.js from 8080 to 80 in line 4

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 module.exports = (function() {
     require('server/globals');
 
-    let port = process.env.PORT || 8080;
+    let port = process.env.PORT || 80;
     let env = process.env.NODE_ENV || 'development';
 
     let server = require('./server/server.js')(inject({
